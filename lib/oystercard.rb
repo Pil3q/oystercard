456b1f7d8353d1c02356a1496d808aabc2146ac2
Patryk Pilecki <ppilecki@icloud.com>
2018-03-27 00:24:21 +0100

    adding money on oyster + tests, raising error while trying to load over the limit

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,6 +1,14 @@
 class Oystercard
+  MAX_BALANCE = 90.0
   attr_reader :balance
-  def initialize(balance = 0)
-    @balance = balance
+  def initialize
+    @balance = 0
   end
+
+  def top_up(money)
+    error_message = "You cannot load more than #{MAX_BALANCE}"
+    raise error_message if balance + money.to_f >= MAX_BALANCE
+    @balance = balance + money.to_f
+  end
+
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -3,4 +3,16 @@ describe Oystercard do
   it 'has a balance equal to 0 once created' do
     expect(subject.balance).to eq 0
   end
+  it 'tops up the card' do
+    subject.top_up(5.5)
+    expect(subject.balance).to eq 5.5
+  end
+  error_message = "You cannot load more than #{Oystercard::MAX_BALANCE}"
+  it 'raises an error when top up limit reached with big single top up' do
+    expect { subject.top_up(91) }.to raise_error error_message
+  end
+  it 'raises an erro when top up limit reached with a few top ups' do
+  10.times { subject.top_up(8) }
+  expect { subject.top_up(11) }.to raise_error error_message
+  end
 end
