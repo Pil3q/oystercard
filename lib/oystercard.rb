60b542e4b893b054b903eda3f6ec230f80242fb5
Patryk Pilecki <ppilecki@icloud.com>
2018-03-27 14:42:39 +0100

    charge while touch out

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,21 +1,23 @@
 # Oystercard!
 class Oystercard
+
   MAX_BALANCE = 90.0
   MINIMUM_FARE = 1.0
+  INITIAL_BALANCE = 0.0
+
   attr_reader :balance, :in_journey
-  def initialize
-    @balance = 0
+
+  def initialize(balance = INITIAL_BALANCE)
+    error_message = "You cannot create a card with more than £#{MAX_BALANCE}"
+    raise error_message if balance > MAX_BALANCE
+    @balance = balance
     @in_journey = false
   end
 
   def top_up(money)
     error_message = "You cannot load more than £#{MAX_BALANCE}"
-    raise error_message if balance + money.to_f > MAX_BALANCE
-    @balance += money.to_f
-  end
-
-  def deduct(money)
-    @balance -= money.to_f
+    raise error_message if balance + money > MAX_BALANCE
+    @balance += money
   end
 
   def touch_in
@@ -26,5 +28,13 @@ class Oystercard
 
   def touch_out
     @in_journey = false
+    deduct(MINIMUM_FARE)
   end
+
+private
+
+  def deduct(money)
+    @balance -= money
+  end
+
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,46 +1,54 @@
 require 'oystercard'
 describe Oystercard do
+  subject(:oystercard)  { described_class.new }
   it 'has a balance equal to 0 once created' do
-    expect(subject.balance).to eq 0
+    expect(oystercard.balance).to eq 0
   end
 
   it 'tops up the card' do
     subject.top_up(5.5)
-    expect(subject.balance).to eq 5.5
+    expect(oystercard.balance).to eq 5.5
   end
 
   error_message = "You cannot load more than £#{Oystercard::MAX_BALANCE}"
 
   it 'raises an error when top up limit reached with big single top up' do
-    expect { subject.top_up(91) }.to raise_error error_message
+    expect { oystercard.top_up(91) }.to raise_error error_message
   end
 
   it 'raises an erro when top up limit reached with a few top ups' do
-    10.times { subject.top_up(8) }
+    10.times { oystercard.top_up(8) }
     expect { subject.top_up(11) }.to raise_error error_message
   end
 end
 describe Oystercard do
-  it 'deduct money from the card' do
-    subject.top_up(10)
-    expect(subject.deduct(5)).to eq 5.0
+  subject(:oystercard)  { described_class.new }
+  # it 'deduct money from the card' do
+  #   oystercard.top_up(10)
+  #   expect(oystercard.deduct(5)).to eq 5.0
+  # end
+describe 'need to up before' do
+  before do
+  oystercard.top_up(5)
   end
-
   it 'is in journey once touch in' do
-    subject.top_up(10)
-    subject.touch_in
-    expect(subject.in_journey).to eq true
+    oystercard.touch_in
+    expect(oystercard.in_journey).to eq true
   end
 
   it 'is not in journey once touch out' do
-    subject.top_up(10)
-    subject.touch_in
-    subject.touch_out
+    oystercard.touch_in
+    oystercard.touch_out
     expect(subject.in_journey).to eq false
   end
+  it 'charger minimum fare while touch out' do
+    expect{ oystercard.touch_out }.to change{ oystercard.balance }.by -Oystercard::MINIMUM_FARE
+  end
+end
 
   it 'raise an error while there is no £ card while touch in' do
     error_message = "You are broken, not even a £#{Oystercard::MINIMUM_FARE} left"
-    expect { subject.touch_in }.to raise_error error_message
+    expect { oystercard.touch_in }.to raise_error error_message
   end
+
 end
